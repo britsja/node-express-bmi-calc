06dc976c58c751b7f44d12b20b9602724569496b
Jan <pcerservices@gmail.com>
2023-06-27 14:47:34 +0200

    Added code for BMI calculator and styling for form

--- bmicalculator.js
+++ bmicalculator.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static('./'))
 
@@ -11,4 +12,23 @@ app.listen(3000, function() {
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", function(req, res) {
+    var weight = parseInt(req.body.weight);
+    var height = parseInt(req.body.height);
+    var bmi = (weight / (height/100 * height/100)).toFixed(1);
+    var message = '';
+    if (bmi > 25) {
+        var message = ' and you are considered <span style="color: red !important;">obese</span>.'
+    } else {
+        var message = ' and you are in a <span style="color:green !important;">healthy</span> range.'
+    }
+    if (isNaN(weight) || isNaN(height) ) {
+        res.send(`<p style="color:red;font-size:40px;">Please enter numbers next time</p>`);
+    } else {
+        var reply = "Your BMI number is "
+        res.send(`<p style="color:blue;font-size:40px;">${reply} ${bmi}${message}</p>`);
+    }
+   
 })
